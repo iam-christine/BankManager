c3ff9897f5a8ea82c7614b2e4c660cb06d256170
iam-christine <tine.chris.myr@gmail.com>
2026-05-01 18:24:51 +0000

    Test du code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Création des objets
-        CheckingAccount c1 = new CheckingAccount("C001", 4000, 500);
-        SavingsAccount s1 = new SavingsAccount("S001", 2000, 3.5);
-        COD cod1 = new COD("D001", 5000, 12);
+        CheckingAccount c1 = new CheckingAccount("C001", 30000, 500);
+        SavingsAccount s1 = new SavingsAccount("S001", 55000, 3.5);
+        COD cod1 = new COD("D001", 7000, 12);
 
         // Accès aux attributs hérités
         System.out.println("CheckingAccount : " + c1.account + " - " + c1.balance);
